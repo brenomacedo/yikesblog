41050e55f264d74cbf7bf0fffbec82141e94926b
brenosekiro <b88660501@gmail.com>
2020-07-09 23:59:25 -0300

    Footer

---
+++ web/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Todos os direitos Reservados - YikesBlog</p>
+        </div>
+    )
+}
+
+export default Footer
