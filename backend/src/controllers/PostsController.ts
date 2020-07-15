52c77eb6730acea3115d0b04e71a842271c819c2
brenosekiro <b88660501@gmail.com>
2020-07-15 17:12:42 -0300

    getPosts

--- backend/src/controllers/PostsController.ts
+++ backend/src/controllers/PostsController.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Put, Delete, Req, Res } from 'routing-controllers'
-import { Request, Response } from 'express'
-import { Repository, getRepository } from 'typeorm'
+import { Request, Response, response } from 'express'
+import { Repository, getRepository, Like } from 'typeorm'
 import Posts from '../models/Posts'
 
 @Controller()
@@ -12,7 +12,7 @@ export default class PostsController {
         this.postsRepository = getRepository(Posts)
     }
 
-    @Post("/posts/create")
+    @Post("/posts")
     async createPost(@Req() request: Request, @Res() response: Response) {
         const post = new Posts()
         post.title = request.body.title
@@ -23,5 +23,30 @@ export default class PostsController {
         await this.postsRepository.save(post)
 
         return response.status(200).send("Post criado com sucesso")
+    }
+
+    @Get("/posts")
+    async getPosts(@Req() request: Request, @Res() response: Response) {
+        const posts = await this.postsRepository.find()
+        return response.status(200).json(posts)
+    }
+
+    @Get("/posts/:id")
+    async getPost(@Req() request: Request, @Res() response: Response) {
+        const post = await this.postsRepository.findByIds([request.params.id], {
+            relations: ["user"]
+        })
+        return response.status(200).json(post[0])
+    }
+
+    @Get("/search")
+    async searchPosts(@Req() request: Request, @Res() response: Response) {
+        const posts = await this.postsRepository.find({
+            where: {
+                title: Like(`%${request.query.post}%`)
+            }
+        })
+
+        return response.status(200).json(posts)
     }
 }
