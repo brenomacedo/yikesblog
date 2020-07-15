d4f672427ee7e49446efac04464d6d946e0ebeda
brenosekiro <b88660501@gmail.com>
2020-07-15 01:00:58 -0300

    signin

--- backend/src/controllers/UsersController.ts
+++ backend/src/controllers/UsersController.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express'
 import { getConnectionManager, Repository, getRepository } from 'typeorm'
 import Users from '../models/Users'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { key } from '../hash.json'
 
 
 @Controller()
@@ -14,8 +16,8 @@ export default class UsersController {
         this.userRepository = getConnectionManager().get().getRepository(Users)
     }
 
-    @Post("/users")
-    async post(@Req() request: Request, @Res() response: Response) {
+    @Post("/users/signup")
+    async createUser(@Req() request: Request, @Res() response: Response) {
        const user = new Users()
 
        const password = await bcrypt.hash(request.body.password, 10)
@@ -28,5 +30,32 @@ export default class UsersController {
        response.send("usuario criado com sucesso")
     }
 
-    
+    @Post("/users/signin")
+    async loginUser(@Req() request: Request, @Res() response: Response) {
+        const user = await this.userRepository.findOne({
+            where: {
+                login: request.body.login
+            }
+        })
+
+        if(!user) {
+            return response.status(400).send("Usuário não encontrado!")
+        }
+
+        if(!await bcrypt.compare(request.body.password, user.password)) {
+            return response.status(400).send("Senha incorreta!")
+        }
+
+        const token = jwt.sign({ id: user.id }, key, { expiresIn: 86400 })
+
+        return response.status(400).json({
+            token, user: {
+                id: user.id,
+                login: user.login,
+                nickname: user.nickname
+            }
+        })
+
+        
+    }
 }
